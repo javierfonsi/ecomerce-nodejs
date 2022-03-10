2ab5979a9bd1c88ef0f23562c3e6bccb56bbb31e
javierfonsi <javier_fonsi@hotmail.com>
2022-03-10 06:39:47 -0500

    v1

---
+++ app.js
@@ -0,0 +1,19 @@
+const express= require('express')
+const { sequelize } = require('./utils/database')
+
+const app = express()
+
+sequelize
+    .authenticate()
+    .then(() => console.log("Database autenthicate"))
+    .catch(error => console.log(error))
+
+sequelize
+    .sync()
+    .then(() => console.log("Database sync"))
+    .catch(error => console.log(error))
+
+app.listen(4000, () => {
+    console.log("Running server");
+})
+
